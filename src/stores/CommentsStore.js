c299366881e1269c9f07f22c41889caaa3a2bb8b
danjarosz <jarosz.daniel@protonmail.com>
2020-11-21 11:53:52 +0100

    feat: implement CommentsStore

---
+++ src/stores/CommentsStore.js
@@ -0,0 +1,25 @@
+import { observable, action } from "mobx";
+
+const NO_RESULT = -1;
+
+export default class CommentStore {
+    @observable
+    comments = [{
+        id: 26738262387,
+        comment: "Super książka!"
+    }];
+
+    @action
+    addComment = comment => this.comments.push(comment);
+
+    @action
+    removeComment = id => {
+        const index = this.comments.findIndex(comment => comment.id === id);
+
+        if (index === NO_RESULT) {
+            return;
+        }
+
+        this.comments = this.comments.filter(comment => comment.id !== id)
+    }
+};
