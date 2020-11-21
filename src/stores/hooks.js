daa0a88397bca73d2a7edc2febc271b92713e0fb
danjarosz <jarosz.daniel@protonmail.com>
2020-11-21 12:24:00 +0100

    feat: implement hooks to get store

---
+++ src/stores/hooks.js
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+
+import { StoreContext } from "./StoreProvider";
+
+//osobny hook do każdego stora z RootStore jest wymagant
+
+export function useCommentsStore() {
+    const rootStore = useContext(StoreContext);
+
+    if (!rootStore) {
+        throw new Error('Nie znaleziono RootStore, sprawdź czy masz Provider, który go dostarcza')
+    };
+
+    return rootStore.commentsStore;
+}
